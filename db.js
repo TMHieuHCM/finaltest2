1068d00f6c46b00213e38df4000bf3fc05df2f29
TMHieu <hieu.truongminh.hcm@gmail.com>
2023-05-08 17:16:20 +0700

    Update code

--- db.js
+++ db.js
@@ -5,11 +5,11 @@ const db = {};
 const connectToDb = () => {
   const client = new MongoClient("mongodb+srv://web_65:7vIC6ZHyo3CpZ7rS@cluster0.sugjv2y.mongodb.net/test");
   client.connect(() => {
-    const database = client.db("your_db_name");
+    const database = client.db("test_3");
     db.inventories = database.collection("inventories");
-    db.orders = database.collection("orders");
+    db.order = database.collection("orders");
     db.users = database.collection("users");
   });
 };
 
-module.exports = { connectToDb, db };
+module.exports = { connectToDb, db }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { connectToDb, db } = require("./db");
+const { connectToDb, db } = require("./db.js");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -7,7 +8,25 @@ app.listen(3000, () => {
   console.log("App is running at 3000");
   connectToDb();
 });
-app.get("/api/inventory", async (req, res) => {
+
+// câu 5
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "Token is required" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, "secret");
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+// câu 2
+app.get("/api/inventory", verifyToken, async (req, res) => {
   try {
     const products = await db.inventories.find().toArray();
     res.json(products);
@@ -16,10 +35,36 @@ app.get("/api/inventory", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+
+// câu 3
 app.get("/api/inventory/low", async (req, res) => {
   try {
-    const products = await db.inventories.find({ instock: { $lt: 100 } }).toArray();
+    const products = await db.inventories
+      .find({ instock: { $lt: 100 } })
+      .toArray();
     res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// câu 4
+app.use(express.json());
+
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await db.users.findOne({
+      username: username,
+      password: password,
+    });
+    if (user) {
+      const token = jwt.sign({ username: username }, "secret",{ expiresIn: "24h" });
+      res.json({ token: token });
+    } else {
+      res.status(401).json({ message: "Invalid username or password" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
